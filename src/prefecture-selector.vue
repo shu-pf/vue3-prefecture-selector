9ca2b422d5acbf8c2b35ed4c2a419d52ddf02ce7
あでり <61904065+shu-pf@users.noreply.github.com>
2021-12-16 03:22:56 +0900

    説明を追加

--- src/prefecture-selector.vue
+++ src/prefecture-selector.vue
@@ -1,17 +1,87 @@
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, SetupContext } from "vue";
 import PrefectureSvg from "./prefecture-svg.vue";
 
 export default defineComponent({
   name: "PrefectureSelector",
+  props: {
+    visibility: {
+      type: Boolean,
+      default: false,
+    },
+    transitionDuration: {
+      type: String,
+      default: "200ms",
+    },
+  },
   components: { PrefectureSvg },
+  setup(_, context: SetupContext) {
+    const onPrefectureClick = (e: { prefectureName: string }) => {
+      context.emit("prefectureClick", e);
+    };
+    const close = () => {
+      context.emit("close");
+    };
+    return {
+      onPrefectureClick,
+      close,
+    };
+  },
 });
 </script>
 
 <template>
-  <div class="prefecture-selector">
-    <PrefectureSvg />
+  <div
+    class="container"
+    :class="{ 'display-block': visibility }"
+    :style="{ 'transition-duration': transitionDuration }"
+  >
+    <div class="overlay">
+      <div class="content">
+        <div>
+          <PrefectureSvg @prefectureClick="onPrefectureClick" />
+        </div>
+        <div class="content__close">
+          <a @click="close">x close</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  display: none;
+}
+.display-block {
+  display: block;
+}
+.overlay {
+  /*　要素を重ねた時の順番　*/
+  z-index: 1;
+
+  /*　画面全体を覆う設定　*/
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+
+  /*　画面の中央に要素を表示させる設定　*/
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.content {
+  z-index: 2;
+  width: 80%;
+  padding: 1em;
+  background: #fff;
+  max-width: 560px;
+}
+.content__close {
+  text-align: right;
+  cursor: pointer;
+}
+</style>
