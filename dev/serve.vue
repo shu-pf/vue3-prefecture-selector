78f22f0209d6dfb582a28dd466c53bab22bd557d
あでり <61904065+shu-pf@users.noreply.github.com>
2021-12-16 03:00:35 +0900

    first commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import PrefectureSelector from '@/prefecture-selector.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    PrefectureSelector
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <prefecture-selector />
+  </div>
+</template>
---
+++ src/prefecture-selector.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import PrefectureSvg from "./prefecture-svg.vue";
+
+export default defineComponent({
+  name: "PrefectureSelector",
+  components: { PrefectureSvg },
+});
+</script>
+
+<template>
+  <div class="prefecture-selector">
+    <PrefectureSvg />
+  </div>
+</template>
+
+<style scoped></style>
